6b25a6f062b1bdb0101161693d251a5dfa7bf39d
BBS17 <blancasbricio977@gmail.com>
2025-05-07 22:04:35 -0700

    Final Product

--- client1/src/LoginPage.java
+++ client1/src/LoginPage.java
@@ -5,16 +5,7 @@ import common.src.UserDAO;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * client1.src.LoginPage: Validates credentials by POSTing to /api/login,
--- server/src/Server.java
+++ server/src/Server.java
@@ -6,7 +6,6 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.Headers;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.nio.charset.StandardCharsets;
